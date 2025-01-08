775f87c7d7a097b151a8ebab48a0dd7b6baabc40
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-01-08 17:03:47 +0100

    refactor: move menubar to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Logo from "@/components/logo";
+import { Menu } from "@/components/home/menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="flex justify-between p-6 items-center">
+          <Logo />
+          <Menu />
+        </div>
+
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { Menu } from "@/components/home/menu";
-import Logo from "@/components/logo";
 import ShareMap from "@/components/map/shareMap";
 
 export default function Home() {
-  return (
-    <div>
-      <div className="flex justify-between p-6 items-center">
-        <Logo />
-        <Menu />
-      </div>
-
-      <ShareMap />
-    </div>
-  );
+  return <ShareMap />;
 }
