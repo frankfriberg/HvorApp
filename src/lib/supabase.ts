fbf9e588d5a303ff01f713c3ba6993db9f5c997c
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-01-17 15:06:11 +0100

    chore: use for of instead of foreach

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -14,9 +14,9 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            );
+            for (const { name, value, options } of cookiesToSet) {
+              cookieStore.set(name, value, options);
+            }
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
